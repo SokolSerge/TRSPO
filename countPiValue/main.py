14c9f54f332fbbde6eb6d7ff5b883623a8bbeb6f
SokolSerge <88060129+SokolSerge@users.noreply.github.com>
2023-01-15 22:07:59 +0300

    Update main.py

--- countPiValue/main.py
+++ countPiValue/main.py
@@ -1,8 +1,36 @@
-import numpy as np
+import time
 from mpi4py import MPI
+import random
 
-if __name__ == '__main__':
-    n_points = int(sys.argv[1])
-    comm = MPI.COMM_WORLD
-    rank = comm.Get_rank()
-    processes_num = comm.Get_size()
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+num_points = 10000
+
+# Main process generates points
+if rank == 0:
+    start_time = time.time()
+    points = [(random.random(), random.random()) for _ in range(num_points)]
+    # Divide the points among the processes
+    chunks = [points[i::size] for i in range(size)]
+else:
+    chunks = None
+
+start_time_local = time.time()
+# Scatter the points to the processes
+points = comm.scatter(chunks, root=0)
+
+# Each process counts the number of points inside the unit circle
+pointsInCircle = sum(x * x + y * y <= 1 for x, y in points)
+
+localPiEstimate = 4 * pointsInCircle / len(points)
+localTime = time.time() - start_time_local
+
+# Gather the results from all processes
+results = comm.gather((localPiEstimate, localTime), root=0)
+
+global_pi_estimate = sum(result[0] for result in results) / size
+global_time = time.time() - start_time
+print("Rank: ", rank, "\nNumber of points: ", num_points, "\nTotal time: ", global_time,
+      "\nPi value: ", global_pi_estimate)
