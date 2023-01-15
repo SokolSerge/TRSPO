9ad0859f9c34c110c52408e7baf89167f80d3cb2
SokolSerge <88060129+SokolSerge@users.noreply.github.com>
2023-01-15 22:19:07 +0300

    modify variable names

--- countPiValue/main.py
+++ countPiValue/main.py
@@ -6,18 +6,18 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
-num_points = 10000
+numPoints = 10000
 
 # Main process generates points
 if rank == 0:
-    start_time = time.time()
-    points = [(random.random(), random.random()) for _ in range(num_points)]
+    startTime = time.time()
+    points = [(random.random(), random.random()) for _ in range(numPoints)]
     # Divide the points among the processes
     chunks = [points[i::size] for i in range(size)]
 else:
     chunks = None
 
-start_time_local = time.time()
+startTimeLocal = time.time()
 # Scatter the points to the processes
 points = comm.scatter(chunks, root=0)
 
@@ -25,12 +25,12 @@ points = comm.scatter(chunks, root=0)
 pointsInCircle = sum(x * x + y * y <= 1 for x, y in points)
 
 localPiEstimate = 4 * pointsInCircle / len(points)
-localTime = time.time() - start_time_local
+localTime = time.time() - startTimeLocal
 
 # Gather the results from all processes
 results = comm.gather((localPiEstimate, localTime), root=0)
 
-global_pi_estimate = sum(result[0] for result in results) / size
-global_time = time.time() - start_time
-print("Rank: ", rank, "\nNumber of points: ", num_points, "\nTotal time: ", global_time,
-      "\nPi value: ", global_pi_estimate)
+globalPiEstimate = sum(result[0] for result in results) / size
+globalTime = time.time() - startTime
+print("Rank: ", rank, "\nNumber of points: ", numPoints, "\nTotal time: ", globalTime,
+      "\nPi value: ", globalPiEstimate)
